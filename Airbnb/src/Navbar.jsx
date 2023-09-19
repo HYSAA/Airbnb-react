dfaed33a49b45caabbc1eacefc24aa44ad713864
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-19 20:51:40 +0800

    asdasd

--- Airbnb/src/Navbar.jsx
+++ Airbnb/src/Navbar.jsx
@@ -17,7 +17,8 @@ export default function Navbar(){
     <div className="text">
     <h1>Online Experiences</h1>
     <p>Join unique interactive activities led by 
-      one-of-a-kind hosts-all without leaving home.
+    </p> 
+     <p>one-of-a-kind hosts-all without leaving home.
     </p>
     </div>
       </>
