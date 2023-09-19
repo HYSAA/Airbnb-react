25fcfd181b32c138c02e7d2f5e40ec959c4aa5b9
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-19 20:44:10 +0800

    asdasd

---
+++ Airbnb/src/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import airbnb from './assets/airbnb-logo.png'
+import photo from './assets/photo-grid.png'
+
+export default function Navbar(){
+
+    return (
+      <>
+
+      <nav className='navbar'>
+    <img src={airbnb} alt="airbnb" />
+    </nav>
+    <div className="menu">
+    <img src={photo} alt="photo" /> 
+  
+    </div>
+    <div className="text">
+    <h1>Online Experiences</h1>
+    <p>Join unique interactive activities led by 
+      one-of-a-kind hosts-all without leaving home.
+    </p>
+    </div>
+      </>
+    )
+}
