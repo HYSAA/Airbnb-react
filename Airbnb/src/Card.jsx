4b470a5963a7bf13b8ad3bb715672a521c0ef23e
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-25 21:03:54 +0800

    asdasdas

--- Airbnb/src/Card.jsx
+++ Airbnb/src/Card.jsx
@@ -4,11 +4,12 @@ import Star from './assets/star.png'
 export default function Card(){
     return(
     <>
-   <div className="pics">
+<div className="pics">
 <img src={Katie} alt="girl" />
 <img src={Star} alt="star"/>
+ <p>(6)</p>
 </div>
-    </>
+    </> 
     )
 
 }
