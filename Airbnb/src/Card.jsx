73839b62047d9a1541f6b304ba1ab2c6890035ea
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-09-28 23:09:11 +0800

    asdasd

--- Airbnb/src/Card.jsx
+++ Airbnb/src/Card.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Katie from './assets/girl.png'
 import Star from './assets/star.png'
-export default function Card(){
-    return(
-    <>
-<div className="pics">
-<img src={Katie} alt="girl" />
-<div className="info">
-<img src={Star} alt="star"/>
-<img src={Katie} alt="asdasd"/>
+export default function Card() {
+    return (
+        <>
+            <div className="pics">
+                <img src={Katie} alt="girl" />
+                <div className="info">
+                    <img src={Star} alt="star" />
+                    <img src={Katie} alt="asdasd" />
 
-<div>
-    </div>
- <p>(6)</p>
- </div>
-</div>
-    </> 
+                    <div>
+                        <div>
+                        </div>
+                    </div>
+                    <p>(6)</p>
+                </div>
+            </div>
+        </>
     )
 
 }
